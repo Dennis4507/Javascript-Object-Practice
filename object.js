c95ae7525f4ec39918673bbbe70aa7fccaeecd92
Dennis4507 <riungudenis63@gmail.com>
2025-04-25 12:43:52 +0200

    Add object manipulation examples to object.js

--- object.js
+++ object.js
@@ -7,3 +7,19 @@ const person = {
   
   delete person.age;
 console.log(`The person's age is ${person.age === undefined ? 'not available' : person.age}.`);
+
+myObj = {
+    name:"John",
+    age:30,
+    myCars: {
+      car1:"Ford",
+      car2:"BMW",
+      car3:"Fiat"
+    }
+  }
+  console.log(myObj.myCars.car1); // Ford
+  console.log(myObj.myCars.car2); // BMW
+    console.log(myObj.myCars.car3); // Fiat
+    console.log(myObj.myCars); // { car1: 'Ford', car2: 'BMW', car3: 'Fiat' }
+    console.log(myObj); // { name: 'John', age: 30, myCars: { car1: 'Ford', car2: 'BMW', car3: 'Fiat' } }
+    console.log(myObj.name); // John
